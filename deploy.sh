a71c0e843799910882fd5dcb62dfa0d5115b40a9
Chris Frewin <frewin.christopher@gmail.com>
2020-07-27 17:36:22 +0200

    finally refactored the webhook url to a repository variable!

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ export SLACK_WEBHOOK_URL=$1 &&
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting CI..."}' $SLACK_WEBHOOK_URL &&
 cd /var/www/skillshare-scp-test && 
 npm install --save serve-handler ngrok forever node-fetch && 
-forever restart index.js &&
+SLACK_WEBHOOK_URL=$1 forever restart index.js &&
 curl -X POST -H 'Content-type: application/json' --data '{"text":"CI complete!"}' $SLACK_WEBHOOK_URL
