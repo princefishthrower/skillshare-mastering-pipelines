1606112b78ef7a98582a2e7a05d8af750e28cc23
Chris Frewin <frewin.christopher@gmail.com>
2020-07-27 14:34:20 +0200

    now we are able to issue commands on the server via SSH!

---
+++ index.js
@@ -0,0 +1,20 @@
+const handler = require('serve-handler');
+const http = require('http');
+const ngrok = require('ngrok');
+
+// Part 1 - serve static content at port 5000
+const server = http.createServer((request, response) => {
+  return handler(request, response);
+})
+ 
+server.listen(5000, () => {
+  console.log('Running at http://localhost:5000');
+});
+
+// Part 2 - use ngrok to expose port 5000 to internet
+async function start() {
+    const url = await ngrok.connect(5000);
+    console.log("Ngrok tunnel to port 5000 exposed at:" + url);
+}
+
+start();
