f9894d65824dd3b6d0f6f78839c4a6c5b91abbd7
Chris Frewin <frewin.christopher@gmail.com>
2020-07-27 17:47:08 +0200

    finally refactored the slack webhook to a repository variable!

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ export SLACK_WEBHOOK_URL=$1 &&
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting CI..."}' $SLACK_WEBHOOK_URL &&
 cd /var/www/skillshare-scp-test && 
 npm install --save serve-handler ngrok forever node-fetch && 
-SLACK_WEBHOOK_URL=$1 forever restart index.js &&
+forever stop index.js && 
+SLACK_WEBHOOK_URL=$1 forever start index.js &&
 curl -X POST -H 'Content-type: application/json' --data '{"text":"CI complete!"}' $SLACK_WEBHOOK_URL
