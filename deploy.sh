cd81750c4e561f13825c06c01f69d4301e44714b
Chris Frewin <frewin.christopher@gmail.com>
2020-07-27 16:16:45 +0200

    adding a slack webhook messaging system!

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting CI..."}' https://hooks.slack.com/services/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 &&
 cd /var/www/skillshare-scp-test && 
 npm install --save serve-handler ngrok forever && 
-forever start index.js
+forever start index.js &&
+curl -X POST -H 'Content-type: application/json' --data '{"text":"CI complete!"}' https://hooks.slack.com/services/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
