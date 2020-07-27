0820774befb9024e3ab9b23ff2b8ea891b1ec90d
Chris Frewin <frewin.christopher@gmail.com>
2020-07-27 16:35:39 +0200

    now we are also able to send messages from a node script!

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Starting CI..."}' https://hooks.slack.com/services/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 &&
 cd /var/www/skillshare-scp-test && 
-npm install --save serve-handler ngrok forever && 
+npm install --save serve-handler ngrok forever node-fetch && 
 forever start index.js &&
 curl -X POST -H 'Content-type: application/json' --data '{"text":"CI complete!"}' https://hooks.slack.com/services/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
--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const handler = require('serve-handler');
-const http = require('http');
-const ngrok = require('ngrok');
+const handler = require("serve-handler");
+const http = require("http");
+const ngrok = require("ngrok");
+const fetch = require("node-fetch");
+
+function sendSlackMessage(message) {
+  fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: message
+      }),
+    }
+  );
+}
 
 // Part 1 - serve static content at port 5000
 const server = http.createServer((request, response) => {
   return handler(request, response);
-})
- 
+});
+
 server.listen(5000, () => {
-  console.log('Running at http://localhost:5000');
+  console.log("Running at http://localhost:5000");
+  sendSlackMessage("Running at http://localhost:5000");
 });
 
 // Part 2 - use ngrok to expose port 5000 to internet
 async function start() {
-    const url = await ngrok.connect(5000);
-    console.log("Ngrok tunnel to port 5000 exposed at: " + url);
+  const url = await ngrok.connect(5000);
+  console.log("Ngrok tunnel to port 5000 exposed at: " + url);
+  sendSlackMessage("Ngrok tunnel to port 5000 exposed at: " + url);
 }
 
 start();
