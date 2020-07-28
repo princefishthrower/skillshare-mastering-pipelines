1f9213f261c174e9e107e0e27bef8cf7837c70bd
Chris Frewin <frewin.christopher@gmail.com>
2020-07-28 11:29:15 +0200

    wow, a full build process with create-react-app and typescript!

---
+++ index.js
@@ -0,0 +1,42 @@
+import env from './src/env/.env.json';
+
+const handler = require("serve-handler");
+const http = require("http");
+const ngrok = require("ngrok");
+const fetch = require("node-fetch");
+
+function sendSlackMessage(message) {
+  fetch(
+    process.env[env.SLACK_WEBHOOK_ENVIRONMENT_KEY],
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: message
+      }),
+    }
+  );
+}
+
+// Part 1 - serve static content at port 5000
+const server = http.createServer((request, response) => {
+  return handler(request, response, {
+      public: "build"
+  });
+});
+
+server.listen(env.port, () => {
+  console.log("Running at http://localhost:" + env.port);
+  sendSlackMessage("Running at http://localhost:" + env.port);
+});
+
+// Part 2 - use ngrok to expose port env.port to internet
+async function start() {
+  const url = await ngrok.connect(env.port);
+  console.log("Ngrok tunnel to port " + env.port + " (Site name: + " + env.siteName + ") exposed at: " + url);
+  sendSlackMessage("Ngrok tunnel to port " + env.port + " (Site name: + " + env.siteName + ") exposed at: " + url);
+}
+
+start();
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import env from './env/.env.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello, my title is {env.siteName}!
+        </p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
